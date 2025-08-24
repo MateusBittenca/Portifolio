7483705799c6522e4c4da6fd04fc298021169cba
MateusBittenca <mpbittenc@gmail.com>
2025-08-24 14:41:37 -0300

    Personalizado título e meta tags do portfolio

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './layout/Header';
 import Footer from './layout/Footer';
 import Home from './components/pages/Home';
 
 function App() {
+  useEffect(() => {
+    // Alterar o título da página
+    document.title = 'Mateus Bittencourt - Portfolio | Fullstack Developer';
+    
+    // Alterar o favicon se quiser (opcional)
+    // const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    // link.type = 'image/x-icon';
+    // link.rel = 'shortcut icon';
+    // link.href = '/favicon.ico';
+    // document.getElementsByTagName('head')[0].appendChild(link);
+  }, []);
+
   return (
     <div className="app">
       <Header />
