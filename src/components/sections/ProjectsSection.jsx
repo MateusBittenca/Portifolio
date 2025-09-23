9cc8d02652c6ca4fc7b5914383588fdaffac713b
MateusBittenca <mpbittenc@gmail.com>
2025-09-22 21:48:49 -0300

    Adicionei algumas imagens

--- src/components/sections/ProjectsSection.jsx
+++ src/components/sections/ProjectsSection.jsx
@@ -3,6 +3,10 @@ import { useTranslation } from '../../hooks/useTranslation';
 import ProjectCard from '../projects/ProjectCard';
 import ProjectModal from '../projects/ProjectModal';
 import portifolioImg from '../../assets/img/portifolio.png';
+import portifolioImg2 from '../../assets/img/portifolio-2.png';
+import portifolioImg3 from '../../assets/img/portifolio-3.png';
+import gastosImg2 from '../../assets/img/sistema de gastos de obra-2.png';
+import gastosImg3 from '../../assets/img/sistema de gastos de obra-3.png';
 import gastosImg from '../../assets/img/Sistema de gastos de obra.png';
 
 
@@ -19,8 +23,8 @@ function ProjectsSection() {
             image: portifolioImg, // Imagem principal (compatibilidade)
             images: [
                 portifolioImg, 
-                portifolioImg,
-                portifolioImg
+                portifolioImg2,
+                portifolioImg3
             ], // Galeria de imagens
             features: t('projects.portfolio.features'),
             challenges: t('projects.portfolio.challenges'),
@@ -34,8 +38,8 @@ function ProjectsSection() {
             image: gastosImg, // Imagem principal (compatibilidade)
             images: [
                 gastosImg,
-                gastosImg,
-                gastosImg
+                gastosImg2,
+                gastosImg3
             ], // Galeria de imagens
             features: t('projects.expenses.features'),
             challenges: t('projects.expenses.challenges'),
